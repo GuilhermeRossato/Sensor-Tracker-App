c66e4688a5d4a3acde53c1e1360fb6cf6c1296b6
Guilherme Rossato <guilhermrossato@gmail.com>
2019-04-07 16:52:40 -0300

    Added port definition from env
    So that the process can be started with an specific port without having to change the code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const path = require("path");
 
 const PROJECTDIR = __dirname;
-
+const port = process.env.PORT || 8080;
 const app = module.exports = express();
 
 // Serve Public Assets
@@ -12,16 +12,16 @@ app.use(express.static(__dirname + '/public'));
 
 // Development-specific Server Configuration
 if (app.settings.env !== "production") {
-	// Log requests
-	app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+    // Log requests
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 }
 
 // Basic Route Test
-app.get("/", function(req, res){
-	res.sendfile("index.html", { root: path.join(PROJECTDIR, "public") });
+app.get("/", function(req, res) {
+    res.sendfile("index.html", { root: path.join(PROJECTDIR, "public") });
 });
 
 // Listen for requests
-app.listen(8080, function() {
-	console.log("Server listening in \"%s\" mode", app.settings.env);
+app.listen(port, function() {
+    console.log("Server listening in \"%s\" mode", app.settings.env);
 });
