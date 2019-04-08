90f9dd47ae03a173f56c0db223daf716350c4868
Guilherme Rossato <guilhermrossato@gmail.com>
2019-04-08 19:49:56 -0300

    Fixed index controller

--- public/app/controllers/index/IndexController.js
+++ public/app/controllers/index/IndexController.js
@@ -1,5 +1,67 @@
 angular.module('TemperatureWatcher')
-.controller('IndexController', function IndexController($scope) {
-	console.log("IndexController function executed");
-	$scope.time = performance.now();
+.controller('IndexController', function IndexController($scope, $interval) {
+
+	let pullTimer;
+	// Handle automatic updates
+	$scope.loadMeasurements = async function loadMeasurements(isFirst = false) {
+		if ($scope.error) {
+			return $interval.cancel(pullTimer);
+		}
+		var response = await fetch("/api/measurements");
+
+		if (!response) {
+			$scope.loadingObject.loading = true;
+			$scope.error = "No response";
+			return false;
+		}
+
+		if (!response.ok) {
+			$scope.loadingObject.loading = true;
+			$scope.error = "Error HTTP Status: "+response.status.toString();
+			return false;
+		}
+
+		try {
+			json = await response.json();
+		} catch (err) {
+			console.error(err);
+			$scope.loadingObject.loading = true;
+			$scope.error = "JSON conversion failed: "+err.stack.toString();
+			return false;
+		}
+
+		if (!json) {
+			return $interval.cancel(pullTimer);
+		}
+		if (!json.error) {
+			$scope.measurementsObject.measurements = json;
+		} else {
+			$scope.loadingObject.loading = true;
+			$scope.error = "Something went wrong";
+		}
+		$scope.loadingObject.loading = false;
+		$scope.timeObject.time = formatDateString(new Date());
+		$scope.$apply();
+	};
+
+	$scope.loadingObject = {
+		loading: true,
+	}
+	$scope.timeObject = {
+		time: 0
+	}
+
+	pullTimer = $interval($scope.loadMeasurements, 5000);
+
+	$scope.$on('$destroy', function(){
+		if (pullTimer) {
+			$interval.cancel(pullTimer);
+		}
+	});
+
+	$scope.loadMeasurements(true);
+
+	$scope.measurementsObject = {
+		measurements: []
+	};
 });
