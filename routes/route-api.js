0cc57f9ac9a209dfd52028bfa94dd8bf7063f2cc
Guilherme Rossato <guilhermrossato@gmail.com>
2019-04-29 13:50:45 -0300

    Made the dummy addition a post request

--- routes/route-api.js
+++ routes/route-api.js
@@ -2,7 +2,7 @@ const ApiController = require("../controllers/ApiController.js");
 
 function applyRoute(app) {
 	// Add another beer name
-	app.get("/api/dummy", ApiController.insertDummyGroup);
+	app.post("/api/dummy", ApiController.insertDummyGroup);
 	app.get("/api/measurements", ApiController.listMeasurements);
 }
 
